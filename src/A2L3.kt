4c0db5aef08a503aeb396ca769c2fb118ab73bea
shindje <aleks.shindje@gmail.com>
2021-08-10 21:18:47 +0300

    A2L3

--- src/A2L3.kt
+++ src/A2L3.kt
@@ -1,5 +1,30 @@
+import java.io.File
 import java.util.*
 
 fun main() {
-//    val scanner = Scanner(System.in)
+    var best = 0
+    var bests = sortedSetOf<String>()
+    val all = mutableMapOf<String, Int>()
+    File("input.txt").forEachLine {
+        val its = it.trim()
+        if (!its.isEmpty()) {
+            val sss = its.split(Regex("\\s+"))
+            for (s in sss) {
+                var sAll = all[s]
+                if (sAll == null)
+                    sAll = 0
+                var newSAll = sAll!!
+                newSAll++
+                all[s] = newSAll
+
+                if (newSAll == best)
+                    bests.add(s)
+                else if (newSAll > best) {
+                    bests = sortedSetOf(s)
+                    best = newSAll
+                }
+            }
+        }
+    }
+    print(bests.first())
 }
