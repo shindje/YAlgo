542dd02affb0c5f268635ce4c356e25486cf6c1e
shindje <aleks.shindje@gmail.com>
2021-08-19 18:31:48 +0300

    Binary search - count

--- src/BinarySearch.kt
+++ src/BinarySearch.kt
@@ -1,6 +1,6 @@
 fun main() {
-    val arr = listOf(1, 4, 4, 6, 8, 9)
-    val search = -1
+    val arr = listOf(1, 4, 4, 6, 6, 6, 8, 9, 9)
+    val search = 9
     var l = 0
     var r = arr.size - 1
     var m = 0
@@ -14,6 +14,29 @@ fun main() {
     if (l == r)
         m = l
     println("$m")
-    if (arr[m] != search)
-        println("not found")
+    var s = -1
+    if (arr[m] >= search)
+        s  = m
+
+    l = 0
+    r = arr.size - 1
+    m = 0
+    while (l < r) {
+        m = (l + r) / 2
+        if (arr[m] > search)
+            r = m
+        else
+            l = m + 1
+    }
+    if (l == r)
+        m = l
+    println("$m")
+    var e = -1
+    if (arr[m] > search)
+        e  = m
+    else
+        e = m + 1
+
+    if (s != -1)
+        println("$search count ${e - s}")
 }
